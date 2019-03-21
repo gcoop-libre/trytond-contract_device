81456440ebc4b6ea646374a8db71668606286377
Luciano Rossi <lukio@gcoop.coop>
2019-03-21 15:56:47 -0300

    change how invoice_type is setted

--- contract.py
+++ contract.py
@@ -52,6 +52,6 @@ class ContractConsumption:
         values = dict(keys)
         device = values['contract'].contract_device
         invoice.pos = values['contract'].pos
-        invoice._set_invoice_type_sequence()
+        invoice.invoice_type = invoice.on_change_with_invoice_type()
         invoice.journal = device.journal
         return invoice
